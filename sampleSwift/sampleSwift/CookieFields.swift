9ab0c3b6d32240125352f803666cbfb9e107145f
alexandrebottero <90895527+alexandrebottero@users.noreply.github.com>
2024-10-14 10:34:30 +0200

    Add brands management (#4)
    * Update test app with new package
    
    * Update documentation

---
+++ sampleSwift/sampleSwift/CookieFields.swift
@@ -0,0 +1,14 @@
+//
+//  CookieFields.swift
+//  sampleSwift
+//
+//  Created by Alexandre Bottero on 03/10/2024.
+//
+
+import Foundation
+
+public enum CookieFields: String, CaseIterable {
+    case cookies = "axeptio_cookies"
+    case allVendors = "axeptio_all_vendors"
+    case authorizedVendors = "axeptio_authorized_vendors"
+}
